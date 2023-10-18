1aa96ce77666933ab78f71982aa547fe80e13c85
Emmanuel Oladiran <oladirane@gmail.com>
2023-10-18 09:04:41 +0100

    script

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const ola = document.getElementById("1");
+const ola2 = document.getElementById("2");
+const ola3 = document.getElementById("3");
 const diran = document.querySelector(".input");
 
-let displayedNumber = null;
+// let displayedNumber = null;
 
 ola.addEventListener("click", () => {
   displayedNumber = 1;
+  diran.textContent = displayedNumber;
+});
+
+ola2.addEventListener("click", () => {
+  displayedNumber = 2;
+  diran.textContent = displayedNumber;
+});
+
+ola3.addEventListener("click", () => {
+  displayedNumber = 3;
   diran.textContent = displayedNumber;
 });
